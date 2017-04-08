06553cdbc9d1ec8427d6d23c78a19f301f8aa8a0
Alexandru Marian Stancioiu <stancioi@adobe.com>
2017-04-09 02:36:32 +0300

    fix

--- rc4.py
+++ rc4.py
@@ -3,12 +3,9 @@ from Crypto.Cipher import ARC4
 def main():
     bytes = 1000
 
-    #key = "key"
-    #cipher = ARC4.new(key)
-    #msg = cipher.encrypt(bytes * '\x00')
-    msg = ''
-    for i in range (0, bytes):
-        msg += str(i)
+    key = "key"
+    cipher = ARC4.new(key)
+    msg = cipher.encrypt(bytes * '\x00')
 
     sample = open("sample.txt", "w")
     sample.write(msg.encode('hex'))
